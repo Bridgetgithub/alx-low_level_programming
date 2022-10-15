55e4a38db2a3f938a6de70cb9bfb55d3424eabcf
Bridget Musunku <bridgetgakii93@gmail.com>
2022-10-15 04:26:07 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 /**
 * main -Print a line of code using puts 
 *
-* Return: always return 0 (success)
-*
+* Return: 0
 */
 
 int main(void)
 {
 
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
