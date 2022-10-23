7898c3a4f6eb68e18ef9bcbab59ee1d68e60b1d6
Bridget Musunku <bridgetgakii93@gmail.com>
2022-10-23 08:51:11 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the number zero to nine
+*
+* Returns: Always 0 (success)
+*/
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; i < 10; i++)
+	_putchar(i + '0');
+	_putchar ('\n');
+}
