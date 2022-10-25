03ca9607b467d36dcb1fb53ee6689e33295be6ae
Bridget Musunku <bridgetgakii93@gmail.com>
2022-10-25 16:04:33 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,11 @@ void print_array(int *a, int n)
 {
 	int t;
 
-		for (t = 0; t < n; t++)
-		{
-			printf("%d", a[t]);
-		}
-		printf("\n");
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n - 1)
+			printf(", ");
+	}
+	printf("\n");
 }
