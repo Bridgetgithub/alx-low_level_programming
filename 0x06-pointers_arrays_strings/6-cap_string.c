88c01a6e1393ceadb72c84eb4dc1a21e5313ce2c
Bridget Musunku <bridgetgakii93@gmail.com>
2022-10-28 09:24:32 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,35 @@
 #include "main.h"
 
 /**
-* cap_string -> capitalizes function
-* @x: string param
-* Return: capitalized version of the string
+* cap_string - capitalizes words
+* @str: string
+* Return: pointer to string
 */
-char *cap_string(char *x)
+char *cap_string(char *str)
 {
-	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int len = 13;
-	int a = 0, i;
+	int i = 0;
 
-	while (x[a])
+	while (str[i])
 	{
-		i = 0;
-		while (i < len)
-		{
-			if ((a == 0 '||' x[a - 1] == spc[i] && (x[a] >= 97 && x[a] <= 122))
-				x[a] = x[a] - 32;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-		}
-		a++;
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+			str[i] -= 32;
+
+		i++;
 	}
-	return (x);
+	return (str);
 }
